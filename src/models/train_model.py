739c2230738f58fa41e8e0f8add6a4e9bf38b2fd
Daniel Hopp <daniel.hopp@metr.systems>
2019-09-25 14:43:21 +0200

    Adds additional images

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -154,18 +154,35 @@ y_dim = images.__getitem__(0)[0].shape[2]
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(channels * x_dim * y_dim, 500)
-        self.fc2 = nn.Linear(500, 200)
-        self.fc3 = nn.Linear(200, n_classes)
+#        self.fc1 = nn.Linear(channels * x_dim * y_dim, 500)
+#        self.fc2 = nn.Linear(500, 200)
+#        self.fc3 = nn.Linear(200, n_classes)
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        self.linear = nn.Sequential(
+            nn.Linear(128 * 128 * 32, 500),
+            nn.ReLU(inplace=True),
+            nn.Linear(500, 200),
+            nn.ReLU(inplace=True),
+            nn.Linear(200, n_classes),
+        )
     
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+ #       x = F.relu(self.fc1(x))
+ #       x = F.relu(self.fc2(x))
+ #       return self.fc3(x)
+        x = self.features(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.linear(x)
+        return x
 
 
-lr = 1e-2
-n_epochs = 10
+lr = 1e-3
+n_epochs = 20
 net = Net()
 if classification:
     criterion = nn.CrossEntropyLoss()
@@ -180,7 +197,7 @@ for epoch in range(n_epochs):
     epoch_loss = []
     for i, data in enumerate(train_loader, 0):
         inputs, labels = data
-        inputs = inputs.view(-1, channels * x_dim * y_dim)
+        #inputs = inputs.view(-1, channels * x_dim * y_dim)
         
         optimizer.zero_grad()
         
@@ -190,10 +207,11 @@ for epoch in range(n_epochs):
             labels = [class_key[i] for i in labels]
             labels = torch.tensor(labels)
             loss = criterion(outputs, labels)
+            epoch_loss.append(np.mean(loss.data.numpy()))
         else:
             loss = criterion(outputs, labels)
+            epoch_loss.append(np.mean(np.sqrt(loss.data.numpy())))
             
-        epoch_loss.append(np.mean(loss.data.numpy()))
         loss.backward()
         optimizer.step()
     total_loss.append(epoch_loss)
